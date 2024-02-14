8bccd4b75d32bc3ae27d1484eb9d7e27f9cec88a
gituser12981u2 <paullwall@protonmail.com>
2024-02-14 09:16:51 -0800

    used reflect to bypass encapsulation to count buckets

--- src/BookWordCounter.java
+++ src/BookWordCounter.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,6 +11,7 @@ import java.util.Scanner;
  * Counts the frequency of words in a book.
  */
 public class BookWordCounter {
+     private static final float LOAD_FACTOR = 0.75f;
     /**
      * Continuously asks the user for a book and a word
      *
@@ -20,6 +22,9 @@ public class BookWordCounter {
         String bookName;
         String bookFilePath;
         boolean fileExists;
+        // String bookName = "hamlet";
+        // String bookFilePath = Paths.get("src/books", bookName + ".txt").toString();
+        // boolean fileExists = Files.exists(Paths.get(bookFilePath));
 
         while (true) {
             do {
@@ -38,8 +43,9 @@ public class BookWordCounter {
                     System.out.println("The book '" + bookName + "' does not exist. Please try again.");
                 }
             } while (!fileExists);
-
+        
             Map<String, Integer> wordCountMap = createWordCountMap(bookFilePath);
+            printHashMapDetails(wordCountMap);
 
             String word;
             do {
@@ -60,6 +66,35 @@ public class BookWordCounter {
             } while (true);
         }
     }
+    
+
+    private static void printHashMapDetails(Map<String, Integer> map) {
+        if (!(map instanceof HashMap)) {
+            System.out.println("The provided map is not an instance of HashMap.");
+            return;
+        }
+    
+        try {
+            HashMap<?, ?> hashMap = (HashMap<?, ?>) map;
+            Class<?> hashMapClass = HashMap.class;
+            Field tableField = hashMapClass.getDeclaredField("table");
+            tableField.setAccessible(true);
+            Object[] table = (Object[]) tableField.get(hashMap);
+    
+            int bucketCount = table != null ? table.length : 0;
+            int size = hashMap.size();
+            float loadFactor = LOAD_FACTOR;
+            float memoryEfficiency = (float) size / (bucketCount * loadFactor);
+    
+            System.out.println("Number of buckets: " + bucketCount);
+            System.out.println("Load factor: " + loadFactor);
+            System.out.println("Current size: " + size);
+            System.out.println("Memory efficiency): " + memoryEfficiency);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
 
     /**
      * Creates a map of word counts from the book
